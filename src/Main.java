66a59d0856623cb4d8e8800ccee75252d8b79f77
Khedd <sinan.ariyurek@gmail.com>
2017-04-23 20:01:10 +0300

    Initial commit 	new file:   .gitignore 	new file:   src/Main.java

---
+++ src/Main.java
@@ -0,0 +1,16 @@
+/**
+ * Main entry pint of project GameStateCreator
+ * Project is used to create a game scenario graph from nodes that have pre conditions and post changes.
+ * From entry point algorithm analyzes which paths the game can take and constricts a graph using these
+ * pre and post.
+ *
+ */
+public class Main {
+    /**
+     * Main method
+     * @param args Not used
+     */
+    public static void main(String[] args){
+        System.out.println("Game State Creator");
+    }
+}
